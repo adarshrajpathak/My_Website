907d129f41e642122cfff1e166ee3c1d6bf76e85
adarshrajpathak <adarsh.raj.pathak.1@gmail.com>
2023-07-23 21:48:35 +0530

    added smooth-scroll functionalities
    Signed-off-by: adarshrajpathak <adarsh.raj.pathak.1@gmail.com>

---
+++ script.js
@@ -0,0 +1,21 @@
+//Code for the Smooth Scroll of Navigation to Target Section
+var Elements=document.querySelectorAll(".smooth-scroll li a");
+// console.log(Elements);
+
+for(var i=0;i<Elements.length;i++){
+    Elements[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        var selectedSection = document.querySelector(this.getAttribute('href'));
+        var scrollInterval=setInterval(function(){
+            w=selectedSection.getBoundingClientRect().top;
+            if(w<=0){  //(window.scrollY>=w)
+                clearInterval(scrollInterval);
+                return;
+            }
+            // console.log(scrollY);
+            // console.log(w);
+            window.scrollBy(0,20);
+            setTimeout(()=>{clearInterval(scrollInterval);},2000);
+        },1);
+    });
+}
